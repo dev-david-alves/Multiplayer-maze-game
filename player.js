eee78770d5ea42a6a7a4e4d8c29e52f6cab78662
dev-david-alves <davidalves9090@gmail.com>
2023-06-15 22:58:07 -0300

    Melhoria na adição e remoção de paredes, textos informativos e fim de jogo adicionados.

--- player.js
+++ player.js
@@ -7,44 +7,66 @@ class Player {
     this.offsetX = offsetX;
     this.offsetY = offsetY;
     this.lastDirection = "r";
+    this.wallsToAdd = 3;
+    this.wallsToDestroy = 3;
   }
 
   move(i, j) {
-    if ((j < 0 && !matrix[this.i][this.j].walls[0]) || (j > 0 && !matrix[this.i][this.j].walls[2])) this.j += j;
-    else if ((i < 0 && !matrix[this.i][this.j].walls[1]) || (i > 0 && !matrix[this.i][this.j].walls[3]))  this.i += i;
+    if (
+      (j < 0 && !matrix[this.i][this.j].walls[0]) ||
+      (j > 0 && !matrix[this.i][this.j].walls[2])
+    )
+      this.j += j;
+    else if (
+      (i < 0 && !matrix[this.i][this.j].walls[1]) ||
+      (i > 0 && !matrix[this.i][this.j].walls[3])
+    )
+      this.i += i;
   }
 
   removeWall(matrix) {
-    if (this.lastDirection == "r" && this.j + 1 < matrix[0].length) {
+    if (this.wallsToDestroy == 0) return matrix;
+
+    if (this.lastDirection == "r" && this.j + 1 < matrix[0].length && matrix[this.i][this.j].walls[2]) {
       matrix[this.i][this.j].walls[2] = false;
       matrix[this.i][this.j + 1].walls[0] = false;
-    } else if (this.lastDirection == "l" && this.j - 1 >= 0) {
+      this.wallsToDestroy -= 1;
+    } else if (this.lastDirection == "l" && this.j - 1 >= 0 && matrix[this.i][this.j].walls[0]) {
       matrix[this.i][this.j].walls[0] = false;
       matrix[this.i][this.j - 1].walls[2] = false;
-    } else if (this.lastDirection == "b" && this.i + 1 < matrix[0].length) {
+      this.wallsToDestroy -= 1;
+    } else if (this.lastDirection == "b" && this.i + 1 < matrix[0].length && matrix[this.i][this.j].walls[3]) {
       matrix[this.i][this.j].walls[3] = false;
       matrix[this.i + 1][this.j].walls[1] = false;
-    } else if (this.lastDirection == "t" && this.i - 1 >= 0) {
+      this.wallsToDestroy -= 1;
+    } else if (this.lastDirection == "t" && this.i - 1 >= 0 && matrix[this.i][this.j].walls[1]) {
       matrix[this.i][this.j].walls[1] = false;
       matrix[this.i - 1][this.j].walls[3] = false;
+      this.wallsToDestroy -= 1;
     }
 
     return matrix;
   }
 
   addWall(matrix) {
-    if (this.lastDirection == "r" && this.j + 1 < matrix[0].length) {
+    if (this.wallsToAdd == 0) return matrix;
+
+    if (this.lastDirection == "r" && this.j + 1 < matrix[0].length && !matrix[this.i][this.j].walls[2]) {
       matrix[this.i][this.j].walls[2] = true;
       matrix[this.i][this.j + 1].walls[0] = true;
-    } else if (this.lastDirection == "l" && this.j - 1 >= 0) {
+      this.wallsToAdd -= 1;
+    } else if (this.lastDirection == "l" && this.j - 1 >= 0 && !matrix[this.i][this.j].walls[0]) {
       matrix[this.i][this.j].walls[0] = true;
       matrix[this.i][this.j - 1].walls[2] = true;
-    } else if (this.lastDirection == "b" && this.i + 1 < matrix[0].length) {
+      this.wallsToAdd -= 1;
+    } else if (this.lastDirection == "b" && this.i + 1 < matrix[0].length && !matrix[this.i][this.j].walls[3]) {
       matrix[this.i][this.j].walls[3] = true;
       matrix[this.i + 1][this.j].walls[1] = true;
-    } else if (this.lastDirection == "t" && this.i - 1 >= 0) {
+      this.wallsToAdd -= 1;
+    } else if (this.lastDirection == "t" && this.i - 1 >= 0 && !matrix[this.i][this.j].walls[1]) {
       matrix[this.i][this.j].walls[1] = true;
       matrix[this.i - 1][this.j].walls[3] = true;
+      this.wallsToAdd -= 1;
     }
 
     return matrix;
--- sketch.js
+++ sketch.js
@@ -1,13 +1,17 @@
-let w = 40;
+let w = 30;
 let numRows = 15;
-let numCols = 15;
+let numCols = 20;
 let matrix = [];
 let stack = [];
+let goal = {
+  x: [2, 10, 18][Math.floor(Math.random() * 3)],
+  y: 13,
+};
 
-let player = new Player(3, 3, w, 30, 5, 5);
+let player = new Player(3, 3, w, w * 0.7, 5, 5);
 
 function setup() {
-  createCanvas(w * numRows + 1, w * numCols + 1);
+  createCanvas(w * numCols + 1, w * numRows + 1);
 
   for (let i = 0; i < numRows; i++) {
     matrix.push([]);
@@ -51,7 +55,7 @@ function createMaze(matrix) {
 
 function drawMaze() {
   for (let i = 0; i < numRows; i++) {
-    for (let j = 0; j < numRows; j++) {
+    for (let j = 0; j < numCols; j++) {
       matrix[i][j].draw();
     }
   }
@@ -102,15 +106,56 @@ function keyPressed() {
   } else if (keyCode === DOWN_ARROW) {
     player.move(1, 0);
     player.lastDirection = "b";
-  } else  if (key === "a") {
-    matrix = player.removeWall(matrix)
-  }  else  if (key === "s") {
-    matrix = player.addWall(matrix)
+  } else if (key === "s") {
+    matrix = player.removeWall(matrix);
+  } else if (key === "a") {
+    matrix = player.addWall(matrix);
   }
+}
+
+function drawPlayerInfor() {
+  textSize(14);
+  textStyle(BOLD);
+  // Criar
+  fill(255);
+  text("Você ainda pode criar", 20, 20);
+  fill(0, 255, 0);
+  text(player.wallsToAdd, 195, 20);
+  fill(255);
+  text("paredes.", 210, 20);
+
+  // Destruir
+  fill(255);
+  text("Você ainda pode destruir", 20, 40);
+  fill(0, 255, 0);
+  text(player.wallsToDestroy, 224, 40);
+  fill(255);
+  text("paredes.", 238, 40);
 }
 
 function draw() {
   background(27, 27, 27);
+  fill(0, 0, 255);
+  noStroke();
+  rect(goal.x * w, goal.y * w, w, w);
+  
   player.draw();
+  drawPlayerInfor();
   drawMaze();
+
+
+  if (player.wallsToAdd == 0) {
+    setTimeout(() => (player.wallsToAdd = 3), 10000);
+  } else if (player.wallsToDestroy == 0) {
+    setTimeout(() => (player.wallsToDestroy = 3), 10000);
+  }
+
+
+  if (player.j == goal.x && player.i == goal.y) {
+    textSize(40);
+    textStyle(BOLD);
+    fill(0, 255, 0);
+    text("Jogador 1 ganhou!", (w * numCols) / 6, (w * numRows) / 2);
+    noLoop();
+  }
 }
